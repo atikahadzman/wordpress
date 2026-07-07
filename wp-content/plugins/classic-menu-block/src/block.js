bd5fe67ae561e71ec5f94a57f7cd5f8f29f00453
atikahadzman <atikah.adzman@gmail.com>
2026-07-08 02:37:37 +0800

    refactor UI

--- wp-content/plugins/classic-menu-block/src/block.js
+++
@@ -1,6 +0,0 @@
-/**
- * Internal dependencies
- */
-import metadata from '../block.json';
-
-export default metadata;
--- wp-content/plugins/gallery-styles/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-block-library', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-hooks', 'wp-i18n'), 'version' => '9207fec39745afadf06c');
